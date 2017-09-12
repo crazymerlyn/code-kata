f59f76008d7f782f81618ff6f8d5912655dec677
CrazyMerlyn <crazy.d.merlyn@gmail.com>
2017-09-12 13:08:19 +0530

    A more extensible version of word splitter

---
+++ 8/extensible.py
@@ -0,0 +1,17 @@
+def perform_split(word, words):
+    for i in range(2, len(word) -1):
+        left = word[:i]
+        right = word[i:]
+        if left in words and right in words:
+            print "%s + %s = %s" % (left, right, word)
+
+words = set()
+with open("/usr/share/dict/words") as f:
+    for line in f:
+        words.add(line.strip())
+
+
+j = 0
+for word in words:
+    perform_split(word, words)
+
